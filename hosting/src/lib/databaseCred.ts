dba1d83a3487c99b54070a13647624f7dfb55e23
jckend <142367196+jckend@users.noreply.github.com>
2024-11-19 16:00:04 -0500

    Update databaseCred.ts

--- hosting/src/lib/databaseCred.ts
+++ hosting/src/lib/databaseCred.ts
@@ -1,10 +1,10 @@
 import type { FirebaseOptions } from 'firebase/app'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCVUVZ1GAjDCb1WUoNz2VxwIenxI81O3VM',
-  authDomain: 'scalar-3ac8c.firebaseapp.com',
-  projectId: 'scalar-3ac8c',
-  messagingSenderId: '529117945658',
-  appId: '1:529117945658:web:f103acf76b0d90804e7a03',
-  measurementId: 'G-D77WVWF8BE',
+  apiKey: 'AIzaSyCVUVZ1GAjDCb1WUoNz2VxwIenxI81O3VM'
+  authDomain: 'scalar-3ac8c.firebaseapp.com'
+  projectId: 'scalar-3ac8c'
+  messagingSenderId: '529117945658'
+  appId: '1:529117945658:web:f103acf76b0d90804e7a03'
+  measurementId: 'G-D77WVWF8BE'
 } as FirebaseOptions
