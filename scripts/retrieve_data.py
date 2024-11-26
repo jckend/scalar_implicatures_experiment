508194c728d3a7d5c95a41ab5c005e048fa7dac0
jckend <142367196+jckend@users.noreply.github.com>
2024-11-26 01:36:44 -0500

    Update retrieve_data.py

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -136,7 +136,7 @@ def _cli():
     )
     parser.add_argument(
         "--collection",
-        dest='exptData-dbug',
+        dest=r"exptData-dbug",
         nargs="+",
         required=True,
         help="Name of the Firestore collections to retrieve",
