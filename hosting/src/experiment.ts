d3254084cbda3b06d2017c8b7635e61f21236945
jckend <142367196+jckend@users.noreply.github.com>
2024-11-26 14:24:06 -0500

    Update experiment.ts

--- hosting/src/experiment.ts
+++ hosting/src/experiment.ts
@@ -699,10 +699,10 @@ var if_node = {
   /* define training procedure */
   const test_procedure0 = {
     timeline: [fixation, question, test, if_node],
-    on_finish: function(){jsPsych.data.displayData();
     timeline_variables: training,
     repetitions: 1,
     randomize_order: false,
+    on_finish: function(){jsPsych.data.displayData();
   }
   timeline.push(test_procedure0)
 
